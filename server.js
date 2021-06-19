14426faae727bbd7682c41cc7dfd7400377bc1b9
Vishal Arora <aroravishal629@gmail.com>
2021-06-19 20:10:59 +0530

    Adding dark mode and delete option

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const ShortUrl = require('./models/shortUrl');
+const express = require('express')
+const mongoose = require('mongoose')
+const ShortUrl = require('./models/shortUrl')
+var favicon = require('serve-favicon')
+const path = require('path')
 const dotenv = require('dotenv')
 dotenv.config()
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 
 const dbName = process.env.DB_NAME
 const dbUser = process.env.DB_USER
 const dbPass = encodeURIComponent(process.env.DB_PASS)
-console.log(process.env.DB_NAME)
+
+var colorScheme = 'light'
 mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@cluster0.vyola.mongodb.net/${dbName}?retryWrites=true&w=majority`,
     {
         useNewUrlParser: true,
@@ -22,18 +25,38 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
 app.use(express.urlencoded({ extended: false }))
 
+app.use(express.static(__dirname + '/public'));
+
+app.use(favicon(__dirname + '/public/images/favicon.ico'))
+
 app.set('view engine', 'ejs');
 
 app.get('/', async (req, res, next) => {
+    const { theme } = req.query
+    colorScheme = theme || 'light'
     let urls = await ShortUrl.find()
-    res.render('index', { urls: urls })
+    res.render('index', { urls: urls, theme: colorScheme || 'light', message: [] })
 })
 
 app.post('/shortUrl', async (req, res, next) => {
-    await ShortUrl.create({
-        longUrl: req.body.shortUrl
-    })
-    res.redirect('/')
+    let { shortUrl } = req.body
+    if (!shortUrl) {
+        let message = ['URL is required']
+        let urls = await ShortUrl.find()
+        res.render('index', { urls: urls, theme: colorScheme || 'light', message: message || [] })
+        return
+    }
+    else {
+        await ShortUrl.create({
+            longUrl: shortUrl
+        })
+    }
+    res.redirect(`/?theme=${colorScheme}`)
+})
+
+app.post('/deleteUrl', async (req, res, next) => {
+    await ShortUrl.findByIdAndDelete(req.query.id)
+    res.redirect(`/?theme=${colorScheme}`)
 })
 
 app.get('/:shortUrl', async (req, res, next) => {
